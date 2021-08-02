6197fa03b2fb1f2cbde737bc4cedf348198f6adc
Leen Barakat <leenbarakat2432001@outlook.com>
2021-08-01 22:46:05 -0700

    add

---
+++ ap.js
@@ -0,0 +1,16 @@
+let name = prompt("what is your name?")
+alert("Hello " + name +" thank you for visiting my website")
+document.write(name);
+
+let old = prompt("How old are you?")
+alert("Your age is " + old +"Nice")
+document.write(old);
+
+let country = prompt("where are you from?")
+alert("Love this country " + country +" thank you for visiting my website")
+document.write(country);
+
+let favType = prompt("what is your favorite type of books?")
+alert("Amazing! " + favType +" thank you for visiting my website")
+document.write(favType);
+
